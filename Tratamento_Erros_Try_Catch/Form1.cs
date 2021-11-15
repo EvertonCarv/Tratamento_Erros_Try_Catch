fd669fd3286952191ed04e4078e474ce52b12558
Everton carvalho <everton.de.carvalho@hotmail.com.br>
2021-11-15 14:07:12 -0300

    Adicionar arquivos de projeto.

---
+++ Tratamento_Erros_Try_Catch/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tratamento_Erros_Try_Catch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int num1, num2, resultador;
+            try
+            {
+                num1 = int.Parse(txtNumero1.Text);
+                num2 = int.Parse(txtNumero2.Text);
+                resultador = num1 + num2;
+                txtResultado.Text = resultador.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Digite apenas valores  númericos", "Catch");
+            }
+        }
+    }
+}
